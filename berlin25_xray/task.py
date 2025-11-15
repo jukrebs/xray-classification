18c42e52e27f5b60b4cc60a3988a8958d1425845
jdobler_local <julian.dobler@web.de>
2025-11-15 01:16:20 +0100

    batchhh

--- berlin25_xray/task.py
+++ berlin25_xray/task.py
@@ -125,7 +125,7 @@ def load_data(
     dataset_name: str,
     split_name: str,
     image_size: int = 224,
-    batch_size: int = 64,
+    batch_size: int = 512,
 ):
     """Load hospital X-ray data.
 
